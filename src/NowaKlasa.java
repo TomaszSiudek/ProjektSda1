7949fa6351f4241fcf4607b054267b63025b1060
Tomasz Siudek <tomaszs1990@gmail.com>
2022-04-02 12:24:40 +0200

    Drugi wpis w historii plików

---
+++ src/NowaKlasa.java
@@ -0,0 +1,17 @@
+/* komentarz wieloliniowy
+* tutaj mogę komentować na
+* wielu liniach*/
+
+/**
+ * Ta klasa jest moją pierwszą klasą!
+ *
+ * Ten komentarz jest częścią dokumentacji!
+ */
+
+public class NowaKlasa {
+    // Do zrobienia
+    // TODO: jak masz coś do zrobienia to zaznacz opcją "TODO"
+    // fixme: fixme: do naprawy
+
+    // Kod powienien się sam dokumentować
+}
